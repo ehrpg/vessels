b6f88fdfb0f351224e0188a4a287d0c024563a91
Johannes Zwirchmayr <Johannes Zwirchmayr>
2015-12-10 23:13:06 +0100

    Ships: Categories for components now show the current and the maximum value of slots.

--- public/js/app.js
+++ public/js/app.js
@@ -54,7 +54,25 @@
 
   var Ship = Vue.extend({
     template: '#ship-template',
-    props: [ 'ship', 'index' ]
+    props: [ 'ship', 'index' ],
+    computed: {
+      armorSlots: function () {
+        return this._sumSlots(this.ship.default.armor)
+      },
+      subsystemSlots: function () {
+        return this._sumSlots(this.ship.default.subsystems)
+      },
+      weaponSlots: function () {
+        return this._sumSlots(this.ship.default.weapons)
+      }
+    },
+    methods: {
+      _sumSlots: function (container) {
+        return container.reduce(function (total, item) {
+          return total + item.slots
+        }, 0)
+      }
+    }
   })
 
   var vm = new Vue({
